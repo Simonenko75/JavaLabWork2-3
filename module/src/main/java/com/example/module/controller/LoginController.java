c6f109fc60e07192a3841eb11ab6c88b308ab0d7
Simonenko75 <simonenkonikita00@gmail.com>
2023-05-24 23:13:30 +0300

    add new modules

--- module/src/main/java/com/example/module/controller/LoginController.java
+++ module/src/main/java/com/example/module/controller/LoginController.java
@@ -1,8 +1,8 @@
 package com.example.module.controller;
 
-import com.example.module.service.LoginService;
 import com.example.module.service.ResponseDTO;
 import com.example.module.service.UserEntityDTO;
+import com.example.module.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
--- sub_module/src/main/java/com/example/sub/module/model/UserEntityDTO.java
+++ sub_module/src/main/java/com/example/sub/module/model/UserEntityDTO.java
@@ -3,8 +3,6 @@ package com.example.sub.module.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Id;
-
 @Getter
 @Setter
 public class UserEntityDTO {
